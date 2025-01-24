5a77f570547187b6dcdba4a6affea45e5d73d864
Bas3L3ss <phamthen.hun060907@gmail.com>
2025-01-24 14:42:31 +0700

    Refactor middleware for protected routes, update layout with new font and theme provider, and remove unused Home page and bruh component.

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Page = async (props: Props) => {
+  //server action onboard the user
+
+  //200||201 dashboard/:id
+
+  return <div>Page</div>;
+};
+
+export default Page;
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,9 @@ const isProtectedRoute = createRouteMatcher([
   "/callback(.*)",
 ]);
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
